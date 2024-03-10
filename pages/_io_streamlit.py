ea4813ccc7b2a3b2faf815881560c1393faa19ac
Wylie CHAN <wylie.chan@gmail.com>
2024-03-10 23:28:53 +0800

    Update _io_streamlit.py

--- pages/_io_streamlit.py
+++ pages/_io_streamlit.py
@@ -944,8 +944,8 @@ if sidebar_selectbox_mode == "V_FinDeriv":
     # C:\Dropbox\_\work\___lib_py_github\wylie-chan-phillips-com-sg\pages\_io_streamlit.py
     # C:\Dropbox\_\work\___lib_py_github\wylie-chan-phillips-com-sg\pages\Fin_Deriv\R_B_ZC_Insur\R_B_ZC_Insur.py
     # from Fin_Deriv.R_B_ZC_Insur import R_B_ZC_Insur
-    import Fin_Deriv.R_B_ZC_Insur.R_B_ZC_Insur
-    
+    # import Fin_Deriv.R_B_ZC_Insur.R_B_ZC_Insur
+
 
     s = "R_B_ZC"
     if section == s:
